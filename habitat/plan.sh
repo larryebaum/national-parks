107c5fb506609965b74ee5af5ae12b1feb84145e
Larry Eichenbaum <larryebaum@getchef.com>
2018-03-23 11:06:24 -0400

    Update plan.sh

--- habitat/plan.sh
+++ habitat/plan.sh
@@ -1,7 +1,7 @@
 pkg_name=national-parks
 pkg_description="A sample JavaEE Web app deployed in the Tomcat8 package"
 pkg_origin=larryebaum
-pkg_version=6.3
+pkg_version=6.3.0
 pkg_maintainer="Larry Eichenbaum <larryebaum@chef.io>"
 pkg_license=('Apache-2.0')
 pkg_deps=(core/tomcat8 core/jre8)
